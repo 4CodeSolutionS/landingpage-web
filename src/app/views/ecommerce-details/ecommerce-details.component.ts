4fc37e7b985c88980f538849d4042cb72129a8bb
iamfelipeS <flipe.silva@terra.com.br>
2024-11-17 21:26:52 -0300

    add: ecommerce-details

---
+++ src/app/views/ecommerce-details/ecommerce-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './ecommerce-details.component.html',
+  styleUrl: './ecommerce-details.component.css'
+})
+export class EcommerceDetailsComponent {
+
+}
