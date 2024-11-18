d70e459efcdbfd53fb677c3071fa56b752607caf
iamfelipeS <flipe.silva@terra.com.br>
2024-11-17 21:07:40 -0300

    add: meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './template/navbar/navbar.component';
 import { HomeComponent } from './views/home/home.component';
@@ -8,6 +8,8 @@ import { ServicesWorkComponent } from './sections/services-work/services-work.co
 import { AboutComponent } from './sections/about/about.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './template/footer/footer.component';
+import { Meta, Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +18,11 @@ import { FooterComponent } from './template/footer/footer.component';
     NavbarComponent,
     HomeComponent,
     ContactComponent,
-    ProductsComponent,
+    //ProductsComponent,
     ServicesWorkComponent,
     AboutComponent,
     FooterComponent,
-    RouterOutlet,
+    // RouterOutlet,
     HttpClientModule,
   ],
   templateUrl: './app.component.html',
@@ -34,8 +36,57 @@ export class AppComponent implements OnInit {
   @ViewChild('servicesWorkSection', { static: true, read: ElementRef }) servicesWorkSection!: ElementRef;
 
 
+  private titleService = inject(Title);
+  private metaService = inject(Meta);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+
   ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updatePageMetaData();
+      }
+    });
   }
+
+  private updatePageMetaData(): void {
+    let route = this.route.firstChild;
+    while (route?.firstChild) {
+      route = route.firstChild;
+    }
+
+    const title = route?.snapshot.data['title'] || 'Minha Loja Online';
+    const description = this.getMetaDescription(route?.snapshot.routeConfig?.path || '');
+    const keywords = this.getMetaKeywords(route?.snapshot.routeConfig?.path || '');
+
+    this.titleService.setTitle(title);
+    this.metaService.updateTag({ name: 'description', content: description });
+    this.metaService.updateTag({ name: 'keywords', content: keywords });
+  }
+
+  private getMetaDescription(route: string): string {
+    switch (route) {
+      case '':
+        return 'Crie sua loja online completa com nossos serviços personalizados.';
+      case 'ecommerce-details':
+        return 'Saiba mais sobre o 3-Commerce, a solução perfeita para seu e-commerce.';
+      default:
+        return 'Minha Loja Online - Soluções personalizadas para seu e-commerce.';
+    }
+  }
+
+  private getMetaKeywords(route: string): string {
+    switch (route) {
+      case '':
+        return 'loja online, criar loja virtual, e-commerce, plataforma de vendas';
+      case 'ecommerce-details':
+        return '3-Commerce, detalhes do e-commerce, criar e-commerce, vendas online';
+      default:
+        return 'e-commerce, loja virtual, criar loja online, vendas na internet';
+    }
+  }
+
   scrollToContact(): void {
     this.contactSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,23 @@ import { HomeComponent } from './views/home/home.component';
 import { EcommerceDetailsComponent } from './views/ecommerce-details/ecommerce-details.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, data: { title: 'Página Inicial' } },
-    { path: 'ecommerce-details', component: EcommerceDetailsComponent, data: { title: '3-Commerce' } }
-];
+    { 
+      path: '', 
+      component: HomeComponent, 
+      data: { 
+        title: 'Página Inicial', 
+        description: 'Crie sua loja online completa com nossos serviços personalizados.', 
+        keywords: 'loja online, criar loja virtual, e-commerce, plataforma de vendas'
+      } 
+    },
+    { 
+      path: 'ecommerce-details', 
+      component: EcommerceDetailsComponent, 
+      data: { 
+        title: '3-Commerce', 
+        description: 'Saiba mais sobre o 3-Commerce, a solução perfeita para seu e-commerce.', 
+        keywords: '3-Commerce, detalhes do e-commerce, criar e-commerce, vendas online'
+      } 
+    }
+  ];
+  
